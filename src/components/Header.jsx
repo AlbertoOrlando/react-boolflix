195bb9af35775b28230856155d8906e5daaa01b3
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-02-28 10:18:27 +0100

    Add Header and DefaultLayout components; implement Searchbar and Results page structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Searchbar from './Searchbar';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Boolflix</h1>
+            <Searchbar></Searchbar>
+        </header>
+    );
+}
